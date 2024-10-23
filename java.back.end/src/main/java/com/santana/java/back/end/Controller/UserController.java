7de630f35975b18df96ca6bfa4064d41855aac68
junior <juniorcarros36@hotmail.com>
2024-10-23 20:40:01 -0300

    first commit

---
+++ java.back.end/src/main/java/com/santana/java/back/end/Controller/UserController.java
@@ -0,0 +1,14 @@
+package com.santana.java.back.end.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // método simples que, quando chamado pelobrowser, retornará uma mensagem para o
+                // usuário
+public class UserController {
+    @GetMapping("/") // que retornará uma mensagem simples para o usuario
+
+    public String getMensagem() {
+        return "Spring boot is working";
+    }
+}
